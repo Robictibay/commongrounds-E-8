b601b19fb75394cf874abb8f29366184b0b636df
Rafael Angelo S Tibayan <rastibayan1028@gmail.com>
2026-03-04 06:52:42 +0800

    Hotfix: Add URL namespacing to localevents and safe fallback for SECRET_KEY

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=xeqav!9=pq=9dk1o7^r!6oq2o1%_23m2a*(#t^oh!jmaxg#uc'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-=xeqav!9=pq=9dk1o7^r!6oq2o1%_23m2a*(#t^oh!jmaxg#uc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

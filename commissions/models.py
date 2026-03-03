064a2e83e1b92e18242d55c5424ed1676a9ad505
Justin <justinheindrich95@gmail.com>
2026-03-03 23:10:44 +0800

    Created the models of Commissions app based on specs

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
+
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"] 
+
+    def __str__(self):
+        return self.name
+    
+class Commissions(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
+    
+    def __str__(self):
+        return self.title   
 # Create your models here.

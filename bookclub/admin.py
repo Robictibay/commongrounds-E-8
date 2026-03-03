c608cff6ca17b821e7e4e340d9ac9f3968c2cf8a
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-04 01:47:28 +0800

    setted up admin page

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,1 +1,13 @@
 from django.contrib import admin
+from .models import Genre, Book
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # This shows the most important info in the admin list view
+    list_display = ('title', 'author', 'publication_year', 'created_on', 'updated_on')
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', )

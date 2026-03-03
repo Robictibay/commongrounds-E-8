b8787a81f1f1a82d289072ab26c81b996c1cecb0
Yuuki Watanabe <yuuki.watanabe@student.ateneo.edu>
2026-03-03 22:35:55 +0800

    privatized secret key

--- commongrounds/settings.py
+++ commongrounds/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=xeqav!9=pq=9dk1o7^r!6oq2o1%_23m2a*(#t^oh!jmaxg#uc'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

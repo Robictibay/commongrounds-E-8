321aab403acd0b85956883d5972c7bcb8c0bdc1c
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-04 15:03:32 +0800

    Initalized merchstore

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,20 +1,3 @@
 from django.contrib import admin
 
-from .models import Product, ProductType
-
-
-class ProductAdmin(admin.ModelAdmin):
-    model = Product
-    search_fields = ('name', 'product_type')
-    list_display = ('name', 'product_type', 'description', 'price')
-    list_filter = ('product_type',)
-
-
-class ProductTypeAdmin(admin.ModelAdmin):
-    model = ProductType
-    search_fields = ('name',)
-    list_display = ('name', 'description',)
-
-
-admin.site.register(Product, ProductAdmin)
-admin.site.register(ProductType, ProductTypeAdmin)
+# Register your models here.

ab074614dfa137f05df63a4c873b74396cb0b189
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-04 16:41:51 +0800

    Added templates for product list and product detail; added admin panel for Product and ProductType

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -3,7 +3,7 @@ from .views import ProductListView, ProductDetailView
 
 app_name = "merchstore"
 
-urlspatterns = [
+urlpatterns = [
     path('items', ProductListView.as_view(), name='item-list'),
     path('item/<int:pk>', ProductDetailView.as_view(), name='item-detail')
 ]

9021bcb1c6723786d7f03fd64a1818153c3a878b
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-04 16:41:51 +0800

    Added templates for product list and product detail; added admin panel for Product and ProductType

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductType
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    search_fields = ('name', 'product_type')
+    list_display = ('name', 'product_type', 'description', 'price')
+    list_filter = ('product_type',)
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+    search_fields = ('name',)
+    list_display = ('name', 'description',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductType, ProductTypeAdmin)

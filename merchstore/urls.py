eb19294576e002c08c32592204eb99f57acff988
francismontesa <francis.montesa@student.ateneo.edu>
2026-03-04 16:07:39 +0800

    Added views for list view and detail views

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import ProductListView, ProductDetailView
 
 app_name = "merchstore"
 
 urlspatterns = [
-    
+    path('items', ProductListView.as_view(), name='item-list'),
+    path('item/<int:pk>', ProductDetailView.as_view(), name='item-detail')
 ]

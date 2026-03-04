dd4469c19a60b2c42a83ba949bf70629132f91ee
Matthew De Villa <raphael.devilla@student.ateneo.edu>
2026-03-04 19:18:21 +0800

    Added url patterns for diyproject app. Paths are projects/ and project/<int:pk>/.

---
+++ diyprojects/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import ProjectListView, ProjectDetailView
+
+urlpatterns = [
+    path('projects', ProjectListView.as_view(), name='project-list'),
+    path('project/<int:pk>', ProjectDetailView.as_view(), name='project-detail'),
+]
+
+app_name = "diyprojects"

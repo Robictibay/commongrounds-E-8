2bc8804bc41629d5be2221fd17d8375469c676b9
Matthew De Villa <raphael.devilla@student.ateneo.edu>
2026-03-04 19:15:59 +0800

    Added the ProjectListView Function and ProjectDetailView Function.

---
+++ diyprojects/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from.models import Project
+# Create your views here.
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'diyprojects/project_list.html'
+    context_object_name = 'projects'
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'diyprojects/project_detail.html'
+    context_object_name = 'project'
+
+

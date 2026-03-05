f0cec6128fd8a189ddaaa7079f9639d8f16cf561
Justin <justinheindrich95@gmail.com>
2026-03-05 17:37:28 +0800

    made the views and admin for Commissions app based on the specs

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CommissionType, Commissions
 
+
+admin.site.register(CommissionType)
+admin.site.register(Commissions)
 # Register your models here.
--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Commissions
+
+def commission_list(request):
+    commissions = Commissions.objects.all()
+    ctx = {"commissions": commissions}
+    return render(request, "commissions/commission_list.html", ctx)
+
+
+def specific_commission(request, pk):
+    commission = Commissions.objects.get(pk=pk)
+    ctx = {"commission": commisssion}
+    return render(request, "commissions/commisssion_specific.html", ctx)
 
 # Create your views here.

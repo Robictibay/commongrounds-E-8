a032c444e3a6d2c6c09938d1a5543067c383fdad
Unu-Lee <justinheindrich95@gmail.com>
2026-05-07 00:07:51 +0800

    Corrected Commissions model based on mistakes from Midterm

--- commissions/models.py
+++ commissions/models.py
@@ -13,10 +13,10 @@ class CommissionType(models.Model):
         return self.name
 
 
-class Commissions(models.Model):
+class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    people_required = models.IntegerField()
+    people_required = models.PositiveIntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -25,7 +25,5 @@ class Commissions(models.Model):
         verbose_name_plural = "Commissions"
 
     def __str__(self):
-        return self.title
+        return self.titleA
 
-
-# Create your models here.

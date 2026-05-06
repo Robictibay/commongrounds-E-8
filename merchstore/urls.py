9e16f1e7a288f0b04cdd5354d0b0ad7e6b20912b
francismontesa <francis.montesa@student.ateneo.edu>
2026-05-06 22:15:49 +0800

    Added Cart View and Transaction List views

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, ProductCreateView
+from .views import (ProductListView, ProductDetailView, ProductCreateView, ProductUpdateview,
+                    CartView, TransactionsList)
 
 app_name = "merchstore"
 
 urlpatterns = [
     path('items', ProductListView.as_view(), name='item-list'),
     path('item/<int:pk>', ProductDetailView.as_view(), name='item-detail'),
-    path('item/add', ProductCreateView.as_view(), name='item-add')
+    path('item/add', ProductCreateView.as_view(), name='item-add'),
+    path('item/<int:pk>/edit', ProductUpdateview.as_view(), name='item-edit'),
+    path('cart', CartView.as_view(), name='cart-view'),
+    path('transactions', TransactionsList.as_view(), name='transaction-list'),
 ]
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
+from django.urls import reverse
 from accounts.mixins import RoleRequiredMixin
+from accounts.models import Profile
 from .models import Product, Transaction
 from .forms import ProductForm, TransactionForm
 
@@ -64,3 +67,36 @@ class ProductCreateView(RoleRequiredMixin, CreateView):
         self.object.owner = self.request.user.profile
         self.object.save()
         return response
+
+
+class ProductUpdateview(RoleRequiredMixin, UpdateView):
+    model = Product
+    template_name = 'merchstore/product_form.html'
+    required_role = 'Market Seller'
+    form_class = ProductForm
+
+
+class CartView(LoginRequiredMixin, ListView):
+    model = Transaction
+    template_name = "merchstore/cart_view.html"
+    redirect_field_name = '/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['items'] = Transaction.objects.filter(buyer__user__exact=self.request.user)
+        unique_owner_ids = Transaction.objects.filter(buyer__user__exact=self.request.user).order_by().values('product__owner').distinct()
+        context['owners'] = Profile.objects.filter(id__in=unique_owner_ids)
+        return context
+
+
+class TransactionsList(LoginRequiredMixin, ListView):
+    model = Transaction
+    template_name = "merchstore/transaction_list.html"
+    redirect_field_name = '/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['items'] = Transaction.objects.filter(product__owner__user__exact=self.request.user)
+        unique_buyer_ids = Transaction.objects.filter(product__owner__user__exact=self.request.user).order_by().values('buyer').distinct()
+        context['buyers'] = Profile.objects.filter(id__in=unique_buyer_ids)
+        return context

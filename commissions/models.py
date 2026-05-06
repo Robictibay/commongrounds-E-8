85f589c4a16cc2805f95235ba301b0b6f4dab586
Unu-Lee <justinheindrich95@gmail.com>
2026-05-07 00:54:44 +0800

    made Job model based on specs

--- commissions/models.py
+++ commissions/models.py
@@ -37,7 +37,29 @@ class Commission(models.Model):
     class Meta:
         ordering = ["created_on"]
         verbose_name_plural = "Commissions"
-
+    
     def __str__(self):
         return self.title
+
+class Job(models.Model):
+    Commission = models.ForeignKey(
+        Commission,
+        on_delete=models.CASCADE
+    )
+    role = models.CharField(max_length=255)
+    manpower_required = models.PositiveIntegerField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('Open', 'Open'),
+            ('Full', 'Full')
+        ],
+        default='Open'
+    )
+
+    class Meta:
+        ordering = ['status', '-manpower_required' 'role']
+    
+    def __str__(self):
+        return self.role
 

49976774c73db32e66651163de752cfb569af138
francismontesa <francis.montesa@student.ateneo.edu>
2026-05-06 17:18:53 +0800

    Added ProductCreateView

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,14 +1,39 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
+from accounts.mixins import RoleRequiredMixin
 from .models import Product
+from .forms import ProductForm
 
 
 class ProductListView(ListView):
     model = Product
     template_name = 'merchstore/product_list.html'
-    context_object_name = 'products'
+    
+    def get_context_data(self, **kwargs):
+        user = self.request.user
+        context = super().get_context_data(**kwargs)
+        if user.is_authenticated:
+            context['all_products'] = Product.objects.exclude(owner__user__exact=user)
+            context['user_products'] = Product.objects.filter(owner__user__exact=user)
+        else:
+            context['all_products'] = Product.objects.all()
+        return context
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'merchstore/product_detail.html'
     context_object_name = 'product'
+
+
+class ProductCreateView(RoleRequiredMixin, CreateView):
+    model = Product
+    template_name = 'merchstore/product_form.html'
+    required_role = 'Market Seller'
+    form_class = ProductForm
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        self.object.owner = self.request.user.profile
+        self.object.save()
+        return response

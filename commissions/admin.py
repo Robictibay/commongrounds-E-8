9cedeb1451599253e7e40fbb61f1bab1d1df802a
Unu-Lee <justinheindrich95@gmail.com>
2026-05-07 01:18:50 +0800

    updated admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import CommissionType, Commissions
+from .models import CommissionType, Commission, Job, JobApplication
 
 
 admin.site.register(CommissionType)
+<<<<<<< HEAD
 admin.site.register(Commissions)
+=======
+admin.site.register(Commission)
+admin.site.register(Job)
+admin.site.register(JobApplication)
+
+>>>>>>> e352953 (updated admin)
